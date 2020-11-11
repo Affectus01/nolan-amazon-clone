0d864550006b6cb6315716f34bfb4bd2bc2e240b
affectus01 <bn_nolan02@hotmail.com>
2020-11-11 11:05:46 -0600

    adding gitignore

--- src/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-  baseURL: "https://us-central1-nolan-amzn-clone-e5e1a.cloudfunctions.net/api",
-  // "http://localhost:5001/nolan-amzn-clone-e5e1a/us-central1/api"
-});
-
-export default instance;
